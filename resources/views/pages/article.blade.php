80150cbf600449162cb0403f0735434c95ec80d9
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2021-12-23 15:36:58 +0000

    Fixing empty object

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -2,58 +2,84 @@
 
 @section('content')
     
-    <div class="container bg-light">
-        <div class="row" style="margin-top: 15px; margin-bottom: 15px;">
+    <div class="article-container container bg-light">
+        <div class="d-flex flex-row my-2 h-100">
             
-            <div class="col-8">
-                {{-- is_author says if the user is the author of the article, so we show the edit button --}}
-                {{ $author['name'] }} <b> {{ $is_author }} </b> 
+            <div class="d-flex flex-column w-75 p-3 text-dark" >
+                <div class="flex-row"> 
+                    <h1> 
+                        {{ $article['title'] }} 
+                        @if ($is_author)
+                            <i class="iconify" style="width: 0.8em; height: 0.7em;" data-icon="bi:pencil-fill"></i>
+                        @endif
+                    </h1>
+                </div>
 
-                <h1 class="text-dark"> {{ $article['title'] }} </h1>
+                <p class="flex-row"> 
+                    @php
+                        $published_at = date('F j, Y', /*, g:i a',*/ strtotime( $article['published_at'] ) )   
+                    @endphp
+                    {{ $published_at }}
+                </p>
 
-                <p> {{ $article['body'] }} </p>
+                <p class="flex-row"> 
+                    @foreach ($tags as $tag)
+                        {{ $tag['name'] }}
+                    @endforeach
+                    <i class="fa fa-thumbs-up ps-5"> {{ $article['likes'] }}</i>
+                    <i class="fa fa-thumbs-down ps-3"> {{ $article['dislikes'] }}</i>
+                    
+                    <i class="iconify ms-5" data-icon="bi:share-fill"></i>
+                </p>
+                
+                <div class="flex-row h-25" style="border: 1px solid red;">
+                    <img class="h-100" src="https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg">
+                </div>
+                
+                <div class="flex-row h-75">
+                    {{ $article['body'] }}
+                </div>
+
             </div>  
 
-            <div class="col-4 text-dark" id="author-container">
-                <div class="row mt-1">
+            <div class="flex-col w-25 p-3 text-dark" id="author-container">
+                <div class="flex-row mt-1">
                     <h2>Author</h2>
                 </div>
                 
-                <div class="row mb-2">
+                <div class="d-flex flex-row mb-3">
                     {{-- {{ $article['thumbnail'] }} --}}
-                    <div class="col-3">
-                        <img id="avatar" src="https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg">
+                    <div class="flex-col w-25" style="margin-right: 1em;">
+                        <img id="avatar" class="h-100" src="https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg">
                     </div>
-
-                    <div class="col-9" id="author-header">
+                    <div class="flex-col w-75" id="author-header" style="padding-bottom: 0;">
                         <h4 class="mb-2"> {{ $author['name'] }} </h4>
-                        <div class="mb-0"> {{ $author['city'] }}, {{ $author['country']->name }} </div>
+                        <p> {{ $author['city'] }}, {{ $author['country']->name }} </p>
                     </div>
                 </div>
 
-                <div class="row mb-1">
+                <div class="flex-row mb-1">
                     <p class="text-secondary">Reputation: {{ $author['reputation'] }} </p>
                 </div>
 
-                <div class="row my-3">
+                <div class="flex-row my-3">
                     <p>Description: </p>
                     <p>{{ $author['description'] }}</p>
                 </div>
 
-                <div class="row my-5">
+                <div class="flex-row my-5">
                     <h3>Areas of Expertise </h3>
                     @foreach ($author['topAreasExpertise'] as $areaExpertise) 
-                        <p> Area Expertise: {{ $areaExpertise['tag_name'] }} </p>
+                        <p>{{ $areaExpertise['tag_name'] }} </p>
                     @endforeach
                 </div>
             </div>
 
+        </div>
+
+        <div class="flex-row">
             @foreach ($comments as $comment)
                 <p> Comentario: {{ $comment['body'] }} </p>
-            @endforeach
-
-            @foreach ($tags as $tag)
-                <p> Tag: {{ $tag['name'] }} </p>
             @endforeach
 
         </div>
