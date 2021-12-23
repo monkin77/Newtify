86bbd9a80f11eaa40f3746bbc9b0d3c53098dd5a
monkin <monkinsmurf@gmail.com>
2021-12-23 20:56:22 +0000

    Working on userInfo section

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -6,36 +6,59 @@ $tagline = 'Que rego amiguinho';
 
 @section('content')
 
-    <div id="userProfileContainer" class="d-flex py-3">
-        <div class="container-fluid h-100" id="userInfo">
-            <div class="row h-100 w-100">
-                <div class="col-6 d-flex flex-column align-items-center h-100">
-                    <div class="h-100">
-                        <img src="https://i2.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1"
-                            id="avatarImg" />
-                        <h2 class="text-center">{{ $user['name'] }}</h2>
-                        <div class="d-flex align-items-center mb-3">
-                            <i class="fa fa-birthday-cake me-3 fa-1x" onclick="console.log('cliked')"></i>
-                            <h5 class="mb-0">April 1st 2001 (20 yo)</h5>
-                        </div>
-                        <div class="d-flex align-items-center mb-3">
-                            <i class="fa fa-flag me-3 fa-1x" onclick="console.log('cliked')"></i>
-                            <h5 class="mb-0">Porto, Portugal</h5>
-                        </div>
+    <div id="userProfileContainer" class="d-flex flex-column">
+        <div class="container-fluid py-3" id="userInfo">
+            <div class="row w-100 mt-5" style="height: 40%">
+                <div class="col-6 d-flex justify-content-center h-100">
+                    <img src="https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png" id="avatarImg" />
+                </div>
+                <div class="col-6 d-flex justify-content-center h-100">
+                    <div class="position-relative">
+                        <h4 style="position: absolute; top: -1.5em;"> Areas of Expertise </h4>
+                        <img src="https://depictdatastudio.com/wp-content/uploads/2017/01/Depict-Data-Studio_Bar-Charts_Vertical-or-Horizontal_Horizontal-1.jpg"
+                            class="h-100" />
                     </div>
                 </div>
-                <div class="col-6">
-                    <h1> Areas of Expertise </h1>
+            </div>
+            <div class="row w-100 mt-5 mb-4">
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <h2 class="text-center  my-0 py-0">{{ $user['name'] }}</h2>
                 </div>
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <i class="fa fa-comment-dots me-3 fa-2x" onclick="console.log('cliked')"></i>
+                    <h3 class="text-center my-0 py-0">Follow</h3>
+                </div>
+            </div>
+            <div class="row w-100 my-2">
+                <div class="col-6 d-flex flex-column align-items-center">
+                    <div class="d-flex align-items-center mb-3">
+                        <i class="fa fa-birthday-cake me-3 fa-1x" onclick="console.log('cliked')"></i>
+                        <h5 class="mb-0">April 1st 2001 (20 yo)</h5>
+                    </div>
+                    <div class="d-flex align-items-center mb-3">
+                        <i class="fa fa-flag me-3 fa-1x" onclick="console.log('cliked')"></i>
+                        <h5 class="mb-0">Porto, Portugal</h5>
+                    </div>
+                </div>
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <div class="d-flex flex-column justify-content-center">
+                        <div style="background-color: green; width: 15em; height: 1.5em; border-radius: 0.7em"> </div>
+                        <h6 class="my-0 py-0 pt-2 ">Reputation: 1500</h6>
+                    </div>
+                </div>
+            </div>
 
-            </div>
-            <div class="h-100 pt-4" id="description">
-                <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at consequat odio. Nullam quis urna
+            <div class="mt-5" id="description">
+                <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at consequat odio. Nullam quis urna
                     commodo, scelerisque massa vulputate, facilisis enim. Mauris facilisis rutrum orci, sed congue enim
-                    tempus sed. Ut dignissim odio in leo mattis, iaculis venenatis nisi ornare. In pellentesque, quam vitae
+                    tempus sed. Ut dignissim odio in leo mattis, iaculis venenatis nisi ornare. In pellentesque, quam
+                    vitae
                     commodo vestibulum, ex magna vulputate massa, quis tincidunt velit metus et magna. Sed a augue urna.
-                    Mauris ac elementum orci. Cras a posuere libero.</h6>
+                    Mauris ac elementum orci. Cras a posuere libero.</h5>
             </div>
+        </div>
+        <div class="container-fluid w-100 d-flex justify-content-center my-5" id="userArticles">
+            <h2 class="border-bottom border-2 border-dark text-center pb-1" id="articlesTitle">Articles</h2>
         </div>
     </div>
 
