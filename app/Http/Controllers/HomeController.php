1d92447a137fa1f9f59efa3a684c69341ac23e99
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-12-26 21:42:57 +0000

    Created function for filtering articles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class HomeController extends Controller
 {
-
     /**
-     * Displays Home page
+     * Displays the home page
      * 
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function show()
     {
@@ -42,5 +43,31 @@ class HomeController extends Controller
         })->sortByDesc('published_at')->take($numArticles);
 
         return $articlesInfo;
+    }
+
+    /**
+     * Return a partial with the filtered articles
+     * 
+     * @param  Illuminate\Http\Request  $request
+     * @return View
+     */
+    public function filter(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'type' => ['nullable', 'string', Rule::in(['trending', 'recent', 'recommended'])],
+            'tags' => 'nullable|array',
+            'tags.*' => 'integer|min:0',
+            'offset' => 'nullable|integer|min:0',
+            'limit' => 'nullable|integer|min:1',
+        ]);
+
+        if ($validator->fails())
+            return response()->json([
+                'status' => 'Bad Request',
+                'msg' => 'Failed to filter articles. Bad request',
+                'errors' => $validator->errors(),
+            ], 400);
+
+        
     }
 }
