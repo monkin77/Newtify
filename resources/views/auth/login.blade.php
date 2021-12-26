7375538bd6a54e6ee434a16e213612c8f70d7f0d
up201706518 <up201706518@fe.up.pt>
2021-12-26 19:01:40 +0000

    qualquer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
-
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
-
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
-
-    <button type="submit">
+<div class="border text-center w-25 bg-light container">
+    <h2 class="modal-titlemx-auto text-center fw-bold" id="exampleModalLabel">Sign In</h2>
+    <form method="POST" action="{{ route('login') }}">
+        {{ csrf_field() }}
+        
+        <label for="email" class="sr-only">Email address</label>
+        <input name ="email" type="email" id="email" class="bg-white" value="{{ old('email') }}" placeholder="Email address" required autofocus>
+        @if ($errors->has('email'))
+            <span class="error">
+                {{ $errors->first('email') }}
+            </span>
+        @endif
+        
+        <label for="password" class="sr-only">Password</label>
+        <input name="password" type="password" id="password" class="bg-white" placeholder="Password" required="">
+        @if ($errors->has('password'))
+            <span class="error">
+                {{ $errors->first('password') }}
+            </span>
+        @endif
+        
+        <div class="checkbox mb-3">
+            <label>
+            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+            </label>
+        </div>
+        <button type="submit" class="btn btn-secondary w-50 fw-bold">
         Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
-</form>
+        </button>
+        <a class="button button-outline w-50" href="{{ route('register') }}">Register</a>
+    </form>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,39 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
 
-    <label for="name">Name</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-    @if ($errors->has('name'))
-      <span class="error">
-          {{ $errors->first('name') }}
-      </span>
-    @endif
+<div class="border text-center w-50 bg-light container-fluid">
+    <h2 class="modal-titlemx-auto text-center fw-bold" id="exampleModalLabel">Sign Up</h2>
+    <form class="form-group" method="post" action="{{ route('register') }}">
+        {{ csrf_field() }}
+        <label for="name">Name</label>
+        <input name="name" class="bg-white w-50" type="text" id="name" placeholder="Enter name" required autofocus>
+        @if ($errors->has('name'))
+        <br>
+        <span class="text-danger error">
+            {{ $errors->first('name') }}
+        </span>
+        @endif
 
-    <label for="email">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @if ($errors->has('email'))
-      <span class="error">
-          {{ $errors->first('email') }}
-      </span>
-    @endif
+        <label for="email">Email address</label>
+        <input name="email" class="bg-white w-50" type="email" id="email" placeholder="Enter email" required>
+        @if ($errors->has('email'))
+        <br>
+        <span class="text-danger error">
+            {{ $errors->first('email') }}
+        </span>
+        @endif
 
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-      <span class="error">
-          {{ $errors->first('password') }}
-      </span>
-    @endif
+        <label for="password">Password</label>
+        <input name="password" class="bg-white w-50" type="password" id="password" placeholder="Password" required>
+        @if ($errors->has('password'))
+        <br>
+        <span class="text-danger error">
+            {{ $errors->first('password') }}
+        </span>
+        @endif
 
-    <label for="password-confirm">Confirm Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
+        <label for="password">Confirm Password</label>
+        <input name="password_confirmation" class="bg-white w-50" type="password" id="password-confirm" placeholder="Confirm Password" required>
 
-    <button type="submit">
-      Register
-    </button>
-    <a class="button button-outline" href="{{ route('login') }}">Login</a>
-</form>
+        <label for="birthDate">Birth Date</label>
+        <input name="birthDate" type="date" id="birthDate" required>
+
+        <label for="country">Country</label>
+        <input name="country" class="bg-white w-50" type="text" id="country" placeholder="Country" required>
+        
+        <label for="avatar">Choose avatar</label>
+        <input name="avatar" type="file" id="avatar">
+        <br>
+        <button type="submit" class="btn btn-secondary w-50 fw-bold">Sign Up</button>
+    </form>
+</div>
+
 @endsection
