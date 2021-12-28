cb565843d5329e98e0337e5a91e5a799f7113064
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2021-12-28 12:34:38 +0000

    Kinda finished the article form

--- resources/views/pages/create_article.blade.php
+++ resources/views/pages/create_article.blade.php
@@ -1,37 +1,40 @@
 @extends('layouts.app')
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" crossorigin="anonymous">
-
 @section('content')
 
-    <div class="container bg-light">
+    <div class="container mb-3 bg-light" id="article-form">
 
         <div class="d-flex flex-row my-2 h-100">
 
-            <div class="d-flex flex-column w-75 p-3 mb-0 text-dark" >
+            <div class="d-flex flex-column w-100 my-2 p-3 h-100">
 
-                <div class="flex-row" >
-                    <h2 class="m-0"> 
-                        <input type="text" id="input-title" class="h-100" placeholder="Insert Title">
-                        {{-- input para titulo do artigo--}}
-                    </h2>
-                </div>
+                <form class="flex-row w-75 h-100">
+                    <div class="flex-row">
+                        <h2 class="m-0"> 
+                            <input type="text" class="h-100" id="input-title" name="input-title" placeholder="Insert Title">
+                            {{-- input para titulo do artigo--}}
+                        </h2>
+                    </div>
 
-                <div class="flex-row mt-3 mb-1"> 
-                    {{-- inputs para 3 tags --}}
-                    <input class="px-3 mx-3" type="text" id="input-tags" data-role="tagsinput">
-                </div>
-                
-                <div class="flex-row h-50 mb-5">
-                    {{-- input para $article['thumbnail']--}}
-                </div>
-                                
-                <div class="flex-row h-75">
-                    {{-- input para $article['body']--}}
-                </div>
+                    <div class="flex-row mt-3 mb-5"> 
+                        {{-- inputs para 3 tags --}}
+                        <input class="px-3 mx-3" type="text" id="input-tags" name="input-tags" data-role="tagsinput" placeholder="Insert Tags">
+                    </div>
+                    
+                    <div class="flex-row">
+                        <input type="file" id="thumbnail" name="thumbnail" accept="image/png, image/jpeg, image/jpg">
+                        {{-- input para $article['thumbnail']--}}
+                    </div>
+                                    
+                    <div class="flex-row h-100">
+                        {{-- input para $article['body']--}}
+                        <textarea id="input-body" name="input-body" rows="15" class="h-100" placeholder="Insert Body"></textarea>
+                    </div>
 
-            </div>  
+                    <button type="button" class="">Create Article</button>
+                </form>
+
+            </div>
 
         </div>
 
