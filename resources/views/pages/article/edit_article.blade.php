b408f762b1605465f4248176b5a493954a9a413a
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2021-12-28 16:45:17 +0000

    Think i finished edit articles form

--- resources/views/pages/article/edit_article.blade.php
+++ resources/views/pages/article/edit_article.blade.php
@@ -10,28 +10,40 @@
 
                 <form class="flex-row w-75 h-100">
                     <div class="flex-row">
+                        <label for="input-title">Edit article's Title</label>
                         <h2 class="m-0"> 
-                            <input type="text" class="h-100" id="input-title" name="input-title" placeholder="Insert Title">
-                            {{-- input para titulo do artigo--}}
+                            <input type="text" 
+                                class="h-100" id="input-title" name="input-title" 
+                                placeholder="{{ $article['title'] }}">
                         </h2>
                     </div>
 
                     <div class="flex-row mt-3 mb-5"> 
-                        {{-- inputs para 3 tags --}}
-                        <input class="px-3 mx-3" type="text" id="input-tags" name="input-tags" data-role="tagsinput" placeholder="Insert Tags">
+                        <label for="input-tags">New article's Tags</label>
+                        <input type="text" 
+                            class="px-3 mx-3" id="input-tags" name="input-tags" 
+                            value="
+                                @foreach($tags as $tag)
+                                    {{ $tag['name'] }},
+                                @endforeach
+                            "
+                            data-role="tagsinput" placeholder="Add Tags">
                     </div>
                     
                     <div class="flex-row">
-                        <input type="file" id="thumbnail" name="thumbnail" accept="image/png, image/jpeg, image/jpg">
-                        {{-- input para $article['thumbnail']--}}
+                        <label for="input-thumbnail">New article's thumbnail</label>
+                        <input type="file" id="input-thumbnail" name="input-thumbnail" accept="image/png, image/jpeg, image/jpg">
                     </div>
                                     
                     <div class="flex-row h-100">
-                        {{-- input para $article['body']--}}
-                        <textarea id="input-body" name="input-body" rows="15" class="h-100" placeholder="Insert Body"></textarea>
+                        <label for="input-body">New article's Body</label>
+                        <textarea 
+                            id="input-body" name="input-body" class="h-100"
+                            rows="15" 
+                            placeholder=" {{ $article['body'] }}"></textarea>
                     </div>
 
-                    <button type="button" class="">Create Article</button>
+                    <button type="button" class="">Update Article</button>
                 </form>
 
             </div>
