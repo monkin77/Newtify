a5c5f2fdd048e51233d2121304f59830f9939159
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2021-12-28 16:20:59 +0000

    Starting edit article form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,6 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" crossorigin="anonymous">
-
     <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
--- resources/views/pages/article/edit_article.blade.php
+++ resources/views/pages/article/edit_article.blade.php
@@ -1,2 +1,43 @@
-<h1>Edit Article Page </h1>
-<p> Editting <b>{{ $article->title }}</b> article </p>
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container mb-3 bg-light" id="article-form">
+
+        <div class="d-flex flex-row my-2 h-100">
+
+            <div class="d-flex flex-column w-100 my-2 p-3 h-100">
+
+                <form class="flex-row w-75 h-100">
+                    <div class="flex-row">
+                        <h2 class="m-0"> 
+                            <input type="text" class="h-100" id="input-title" name="input-title" placeholder="Insert Title">
+                            {{-- input para titulo do artigo--}}
+                        </h2>
+                    </div>
+
+                    <div class="flex-row mt-3 mb-5"> 
+                        {{-- inputs para 3 tags --}}
+                        <input class="px-3 mx-3" type="text" id="input-tags" name="input-tags" data-role="tagsinput" placeholder="Insert Tags">
+                    </div>
+                    
+                    <div class="flex-row">
+                        <input type="file" id="thumbnail" name="thumbnail" accept="image/png, image/jpeg, image/jpg">
+                        {{-- input para $article['thumbnail']--}}
+                    </div>
+                                    
+                    <div class="flex-row h-100">
+                        {{-- input para $article['body']--}}
+                        <textarea id="input-body" name="input-body" rows="15" class="h-100" placeholder="Insert Body"></textarea>
+                    </div>
+
+                    <button type="button" class="">Create Article</button>
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
