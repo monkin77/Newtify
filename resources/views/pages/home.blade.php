d35052a0a058558afde15a75d86336c27152e1e6
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-12-30 17:21:57 +0000

    Added route to create article form

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @php
-$tagline = 'Que rego amiguinho'
+$tagline = 'Others will decide your faith'
 @endphp
 
 {{-- ------------------------------------------------------------------------------------ --}}
 @section('create-article')
 
-<div id="homepage" class="d-flex justify-content-center py-3">
+<div id="homepage" class="d-flex justify-content-center pb-3">
     <div class="d-flex flex-grow-1 justify-content-center createArticleContainer">
         <div id="createArticle" class="position-relative d-flex flex-column align-items-center" >
             <h1> Create Your Own Article </h1>
             <h3> {{ $tagline }} </h3>
-            <div class="addIcon"> 
-                <i class="fas fa-plus-circle fa-4x" onclick="console.log('Clicked')"></i>
-            </div>   
+            <a class="addIcon btn" href="{{ route('newArticlePage') }}"> 
+                <i class="fas fa-plus-circle fa-4x"></i>
+            </a>   
         </div>
     </div>
 </div>
