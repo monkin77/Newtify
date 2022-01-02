6155e6f5e88605fc48fd3e723462307bdacff804
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-02 22:37:32 +0000

    Changed class

--- resources/views/pages/admin/manageTags.blade.php
+++ resources/views/pages/admin/manageTags.blade.php
@@ -10,7 +10,7 @@
         <div class="border bg-light me-3 statusContainer col">
             <h4 class="mt-5">Accepted Tags</h4>
             @foreach ($tags_accepted as $tag)
-                <div class="mt-5 pb-3 pt-5 bg-light mb-5 tagContainer">
+                <div class="mt-5 pb-3 pt-5 bg-light mb-5 manageTagContainer">
                     <div id="stateButton" class="d-flex align-items-center">
                         <h5 class="mx-3 my-0 py-0 w-75">{{ $tag['name'] }}</h5>
                         <button type="button" onclick="removeTag(this, {{ $tag['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
@@ -24,7 +24,7 @@
         <div class="border bg-light statusContainer col">
             <h4 class="mt-5">Pending Tags</h4>
             @foreach ($tags_pending as $tag)
-                <div class="mt-5 pb-3 pt-5 bg-light mb-5 tagContainer">
+                <div class="mt-5 pb-3 pt-5 bg-light mb-5 manageTagContainer">
                     <div id="stateButton" class="d-flex align-items-center">
                         <h5 class="mx-3 my-0 py-0 w-75">{{ $tag['name'] }}</h5>
                         <button type="button" onclick="acceptTag(this, {{ $tag['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
@@ -41,7 +41,7 @@
         <div class="border bg-light mx-3 statusContainer col">
             <h4 class="mt-5">Rejected Tags</h4>
             @foreach ($tags_rejected as $tag)
-                <div class="mt-5 pb-3 pt-5 bg-light mb-5 tagContainer">
+                <div class="mt-5 pb-3 pt-5 bg-light mb-5 manageTagContainer">
                     <div id="stateButton" class="d-flex align-items-center">
                         <h5 class="mx-3 my-0 py-0 w-75">{{ $tag['name'] }}</h5>
                         <button type="button" onclick="acceptTag(this, {{ $tag['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
