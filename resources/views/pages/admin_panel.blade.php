9f7fe11849ec40e14e472d9524b668153e908599
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-02 12:22:34 +0000

    Admin Page with icones and redirect

--- resources/views/pages/admin_panel.blade.php
+++ resources/views/pages/admin_panel.blade.php
@@ -5,17 +5,25 @@
 
 <div class="my-5 container">
     <div class="d-flex flex-row row-cols-3">
-        <div class="d-flex align-items-center flex-column border shadow bg-light me-3 linkContainer col">
-            <i class="fas fa-users cir iconSize"></i>
-            <a class="text-secondary" href="{{ url('/admin/reports') }}">Manage Suspensions</a>
+        <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
+            <div class="linkContainer h-50 w-100">
+                <i class="fas fa-users-slash suspensionsIcon shadow-lg rounded-circle"></i>
+            </div>
+            <a class="manageLink text-secondary" href="{{ url('/admin/suspensions') }}">Manage User Suspensions</a>
         </div>
-        <div class="d-flex text-center flex-column border shadow bg-light linkContainer col">
-            <i class="fas fa-users iconSize"></i>
-            <a class="text-secondary" href="{{ url('/admin/reports') }}">Manage Reports</a>
-        </div> 
-        <div class="d-flex text-center flex-column border shadow bg-light mx-3 linkContainer col">
-            <i class="fas fa-users iconSize"></i>
-            <a class="text-secondary" href="{{ url('/admin/reports') }}">Manage Tags</a>
+        
+        <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
+            <div class="linkContainer h-50 w-100">
+                <i class="fas fa-ban reportsIcon shadow-lg rounded-circle"></i>
+            </div>
+            <a class="manageLink text-secondary" href="{{ url('/admin/reports') }}">Manage Reports</a>
+        </div>
+        
+        <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
+            <div class="linkContainer h-50 w-100">
+                <i class="tagsIcon shadow-lg rounded-circle fas fa-tags"></i>
+            </div>
+            <a class="manageLink text-secondary" href="{{ url('/admin/tags') }}">Manage Tags</a>
         </div>
     </div>
 </div>
