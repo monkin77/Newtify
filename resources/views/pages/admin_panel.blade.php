21d3f26ed55d2f89efbf87ecdf7e8f7f49a68dc5
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-02 14:55:03 +0000

    Trying to make icons border but failing

--- resources/views/pages/admin_panel.blade.php
+++ resources/views/pages/admin_panel.blade.php
@@ -6,8 +6,10 @@
 <div class="my-5 container">
     <div class="d-flex flex-row row-cols-3">
         <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
-            <div class="linkContainer h-50 w-100">
-                <i class="fas fa-users-slash suspensionsIcon shadow-lg rounded-circle"></i>
+            <div class="linkContainer w-100">
+                <div class="iconContainer shadow-lg rounded-circler">
+                    <i class="fas fa-users-slash suspensionsIcon"></i>
+                </div>
             </div>
             <a class="manageLink text-secondary" href="{{ url('/admin/suspensions') }}">Manage User Suspensions</a>
         </div>
