9e9466f53e9c2ec86f63bf53043ec18276dc9605
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-02 17:56:22 +0000

    Changed display of buttons in Manage Tags Page

--- resources/views/pages/tags.blade.php
+++ resources/views/pages/tags.blade.php
@@ -11,9 +11,11 @@
             <h4 class="mt-5">Accepted Tags</h4>
             @foreach ($tags_accepted as $tag)
                 <div class="mt-5 pb-3 pt-5 bg-light mb-5 tagContainer">
-                    <p>{{ $tag['name'] }}</p>
-                    <div id="stateButton">
-                        <button type="button" onclick="removeTag(this, {{ $tag['id'] }})" class="btn btn-lg btn-danger">Delete</button>
+                    <div id="stateButton" class="d-flex align-items-center">
+                        <h5 class="mx-3 my-0 py-0 w-75">{{ $tag['name'] }}</h5>
+                        <button type="button" onclick="removeTag(this, {{ $tag['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
+                            <i class="fas fa-trash fa-2x mb-2 text-danger"></i>
+                        </button>
                     </div>
                 </div>
             @endforeach
@@ -23,10 +25,14 @@
             <h4 class="mt-5">Pending Tags</h4>
             @foreach ($tags_pending as $tag)
                 <div class="mt-5 pb-3 pt-5 bg-light mb-5 tagContainer">
-                    <p>{{ $tag['name'] }}</p>
-                    <div id="stateButton">
-                        <button type="button" onclick="acceptTag(this, {{ $tag['id'] }})" class="btn btn-lg btn-success">Accept</button>
-                        <button type="button" onclick="rejectTag(this, {{ $tag['id'] }})" class="btn btn-lg btn-danger">Reject</button>
+                    <div id="stateButton" class="d-flex align-items-center">
+                        <h5 class="mx-3 my-0 py-0 w-75">{{ $tag['name'] }}</h5>
+                        <button type="button" onclick="acceptTag(this, {{ $tag['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
+                            <i class="fas fa-check fa-2x mb-2 text-success"></i>
+                        </button>
+                        <button type="button" onclick="rejectTag(this, {{ $tag['id'] }})" class="my-0 mx-1 py-0 btn btn-lg btn-tranparent">
+                            <i class="fas fa-times fa-2x mb-2 text-danger"></i>
+                        </button>
                     </div>
                 </div>
             @endforeach
@@ -36,9 +42,11 @@
             <h4 class="mt-5">Rejected Tags</h4>
             @foreach ($tags_rejected as $tag)
                 <div class="mt-5 pb-3 pt-5 bg-light mb-5 tagContainer">
-                    <p>{{ $tag['name'] }}</p>
-                    <div id="stateButton">
-                        <button type="button" onclick="acceptTag(this, {{ $tag['id'] }})" class="btn btn-lg btn-success">Accept</button>
+                    <div id="stateButton" class="d-flex align-items-center">
+                        <h5 class="mx-3 my-0 py-0 w-75">{{ $tag['name'] }}</h5>
+                        <button type="button" onclick="acceptTag(this, {{ $tag['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
+                            <i class="fas fa-check fa-2x mb-2 text-success"></i>
+                        </button>
                     </div>
                 </div>
             @endforeach
