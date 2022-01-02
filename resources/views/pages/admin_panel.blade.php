8144a62590dfb5fd733a63f9a3b092c84202847d
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-02 16:57:56 +0000

    Fixed Manage Links Css

--- resources/views/pages/admin_panel.blade.php
+++ resources/views/pages/admin_panel.blade.php
@@ -5,27 +5,39 @@
 
 <div class="my-5 container">
     <div class="d-flex flex-row row-cols-3">
-        <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
-            <div class="linkContainer w-100">
-                <div class="iconContainer shadow-lg rounded-circler">
-                    <i class="fas fa-users-slash suspensionsIcon"></i>
+        <a href="{{ url('/admin/suspensions') }}" class="text-secondary">
+            <div class="d-flex flex-column manageContainer border col bg-light me-3 h-100">
+                <div class="linkContainer w-100">
+                    <div class="iconContainer shadow-lg rounded-circle">
+                        <i class="fas fa-users-slash suspensionsIcon"></i>
+                    </div>
+                </div>
+                <div class="manageLink">
+                    Manage User Suspensions
                 </div>
             </div>
-            <a class="manageLink text-secondary" href="{{ url('/admin/suspensions') }}">Manage User Suspensions</a>
+        </a>
+        
+        <div class="d-flex flex-column manageContainer border col bg-light me-3 h-100">
+            <div class="linkContainer">
+                <div class="iconContainer shadow-lg rounded-circle">
+                    <i class="fas fa-ban reportsIcon"></i>
+                </div>
+            </div>
+            <div class="manageLink">
+                <a class="text-secondary" href="{{ url('/admin/reports') }}">Manage Reports</a>
+            </div>
         </div>
         
-        <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
-            <div class="linkContainer h-50 w-100">
-                <i class="fas fa-ban reportsIcon shadow-lg rounded-circle"></i>
+        <div class="d-flex flex-column manageContainer border col bg-light me-3 h-100">
+            <div class="linkContainer">
+                <div class="iconContainer shadow-lg rounded-circle">
+                    <i class="tagsIcon fas fa-tags"></i>
+                </div>
             </div>
-            <a class="manageLink text-secondary" href="{{ url('/admin/reports') }}">Manage Reports</a>
-        </div>
-        
-        <div class="d-flex flex-column border shadow col bg-light me-3 h-100">
-            <div class="linkContainer h-50 w-100">
-                <i class="tagsIcon shadow-lg rounded-circle fas fa-tags"></i>
+            <div class="manageLink">
+                <a class="text-secondary" href="{{ url('/admin/tags') }}">Manage Tags</a>
             </div>
-            <a class="manageLink text-secondary" href="{{ url('/admin/tags') }}">Manage Tags</a>
         </div>
     </div>
 </div>
