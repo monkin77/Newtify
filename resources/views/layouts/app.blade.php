cf7adc927a5e6d1531695cffb2785f62ac545a1a
monkin <monkinsmurf@gmail.com>
2022-01-11 23:34:17 +0000

    Redesigned homepage filters, propose new tag, added custom colors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
         integrity="sha384-8iuq0iaMHpnH2vSyvZMSIqQuUnQA7QM+f6srIdlgBrTSEyd//AWNMyEaSF2yPzNQ" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
+    <link href="{{ asset('css/AppTheme.css') }}" rel="stylesheet">
     <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,27 +5,27 @@
 {{-- ------------------------------------------------------------------------------------ --}}
 
 @section('filters')
-    <section id="filterSection" class="d-flex flex-row border mb-4 py-3">
+    <section id="filterSection" class="d-flex flex-row border border-secondary mb-4 py-3">
         <div class="btn-group btn-group-toggle me-auto" data-toggle="buttons">
             <input type="radio" class="btn-check" name="filterType" id="trending" autocomplete="off" checked>
-            <label class="filter-button btn btn-outline-secondary ms-4 my-auto" for="trending">
+            <label class="filter-button btn btn-outline-primary ms-4 my-auto" for="trending">
                 <i class="fas fa-fire-alt mt-2"></i> <span class="mx-2">Trending</span>
             </label>
 
             <input type="radio" class="btn-check" name="filterType" id="recent" autocomplete="off">
-            <label class="filter-button btn btn-outline-secondary btn-lg ms-4 my-auto" for="recent">
+            <label class="filter-button btn btn-outline-primary btn-lg ms-4 my-auto" for="recent">
                 <i class="fas fa-history mt-2"></i> <span class="mx-2">Recent</span>
             </label>
 
             @if (Auth::check())
                 <input type="radio" class="btn-check" name="filterType" id="recommended" autocomplete="off">
-                <label class="filter-button btn btn-outline-secondary btn-lg ms-4 my-auto" for="recommended">
+                <label class="filter-button btn btn-outline-primary btn-lg ms-4 my-auto" for="recommended">
                     <i class="far fa-star mt-2"></i> <span class="mx-2">Recommended</span>
                 </label>
             @endif
         </div>
 
-        <i class="fa fa-tag filter-tag mt-2 me-4"></i>
+        <i class="fa fa-tag filter-tag mt-2 me-4 text-purple"></i>
     </section>
 @endsection
 
@@ -41,23 +41,20 @@
 
 @section('load-more')
     <div id="load-more">
-
         <button onclick="loadMoreHome()">Load more</button>
-
     </div>
 @endsection
 
 {{-- ------------------------------------------------------------------------------------ --}}
 
 @section('propose-tag')
-
     <section class="home-section d-flex justify-content-center mt-3">
         <div class="d-flex flex-grow-1 justify-content-center home-container">
-            <div id="proposeTag" class="position-relative d-flex flex-column align-items-center">
+            <div id="proposeTag" class="position-relative d-flex flex-column align-items-center text-light">
                 <h1 class="mb-2">
-                    <i class="fa fa-tag fa-sm fa-flip-horizontal px-2"></i>
+                    <i class="fa fa-tag fa-sm fa-flip-horizontal px-2 text-purple-dark"></i>
                     Propose a new Tag
-                    <i class="fa fa-tag fa-sm px-2"></i>
+                    <i class="fa fa-tag fa-sm px-2 text-purple-dark"></i>
                 </h1>
                 <h4 class="mb-5"> Help us improve on getting more variety of content </h4>
 
