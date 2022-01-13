5edbbab690442eea606ee0e14dd6590f62a479ae
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-01-13 14:21:27 +0000

    Created abstract notification event

--- app/Events/ArticleLike.php
+++ app/Events/ArticleLike.php
@@ -2,20 +2,8 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
-class ArticleLike implements ShouldBroadcast
+class ArticleLike extends Notification
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public $username;
-    public $avatar;
     public $article_id;
     public $article_title;
 
@@ -26,20 +14,9 @@ class ArticleLike implements ShouldBroadcast
      */
     public function __construct($username, $avatar, $article_id, $article_title)
     {
-        $this->username = $username;
-        $this->avatar = $avatar;
+        parent::__construct($username, $avatar);
         $this->article_id = $article_id;
         $this->article_title = $article_title;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return ['notifications'];
     }
 
     /**
--- public/js/pusher.js
+++ public/js/pusher.js
@@ -4,4 +4,4 @@ const pusher = new Pusher('26ec0b6978ee54d90d56', {
 });
 
 const notificationChannel = pusher.subscribe('notifications');
-notificationChannel.bind('article-like', (data) => console.log("Like no artigo bro"));
+notificationChannel.bind('article-like', (data) => console.log("Like no artigo bro", data));
