17c70e0bb2d1c5c9a7be88b2cb1609fa3a6ec22f
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-21 19:31:15 +0000

    added title to followedUsers page

--- resources/views/pages/user/followedUsers.blade.php
+++ resources/views/pages/user/followedUsers.blade.php
@@ -4,6 +4,8 @@
     <script type="text/javascript" src={{ asset('js/user.js') }}></script>
 @endsection
 
+@section('title', "- Followed Users")
+
 @section('content')
 
     <section id="users" class="container">
