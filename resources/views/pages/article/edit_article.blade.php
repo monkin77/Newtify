176d01e7beff96b240ae3425c0d49ea7034f3170
Rui Filipe Teixeira Alves <up201905853@fe.up.pt>
2022-01-21 18:47:36 +0000

    Fixed thumbnail preview in edit article

--- resources/views/pages/article/edit_article.blade.php
+++ resources/views/pages/article/edit_article.blade.php
@@ -3,6 +3,7 @@
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script type="text/javascript" src={{ asset('js/user.js') }}></script>
     <script type="text/javascript" src=" {{ asset('js/select2tags.js') }}"> </script>
 @endsection
 
@@ -61,7 +62,7 @@
 
                         <div id="avatarPreviewContainer" class="d-flex flex-column align-items-center">
                             <img class="col-8 col-md-6 mb-3" src={{ isset($article['thumbnail']) 
-                                ? asset('storage/thumbnails' . $article['thumbnail'])
+                                ? asset('storage/thumbnails/' . $article['thumbnail'])
                                 : $articleImgPHolder }}
                                 id="avatarPreview" onerror="this.src='{{ $articleImgPHolder }}'" />
                             
